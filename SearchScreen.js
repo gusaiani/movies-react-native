832db7cb9c48344c08fa059e0b58100d7674685b
Gustavo Saiani <gustavo@poe.ma>
2016-03-01 09:48:41 -0300

    Start SearchScreen

---
+++ SearchScreen.js
@@ -0,0 +1,137 @@
+'use strict';
+
+var React = require('react-native');
+var {
+  ActivityIndicatorIOS,
+  ListView,
+  Platform,
+  ProgressBarAndroid,
+  StyleSheet,
+  Text,
+  View,
+} = React;
+var TimerMixin = require('react-timer-mixin');
+
+var invariant = require('invariant');
+var dismissKeyboard = require('dismissKeyboard');
+
+var MovieCell = require('./MovieCell');
+var MovieScreen = require('./MovieScreen');
+var SearchBar = require('SearchBar');
+
+/**
+ * This is for demo purposes only, and rate limited.
+ * In case you want to use the Rotten Tomatoes' API on a real app you should
+ * create an account at http://developer.rottentomatoes.com/
+ */
+var API_URL = 'http://api.rottentomatoes.com/api/public/v1.0/';
+var API_KEYS = [
+  '7waqfqbprs7pajbz28mqf6vz',
+  // 'y4vwv8m33hed9ety83jmv52f', Fallback api_key
+];
+
+// Results should be cached keyed by the query
+// with values of null meaning "being fetched"
+// and anything besides null and undefined
+// as the result of a valid query
+var resultsCache = {
+  dataForQuery: {},
+  nextPageNumberForQuery: {},
+  totalForQuery: {},
+};
+
+var LOADING = {};
+
+var SearchScreen = React.createClass({
+  mixins: [TimerMixin],
+
+  timeoutID: (null: any),
+
+  getInitialState: function() {
+    return {
+      isLoading: false,
+      isLoadingTail: false,
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      }),
+      filter: '',
+      queryNumber: 0,
+    };
+  },
+
+  componentDidMount: function() {
+    this.searchMovies('');
+  },
+
+  _urlForQueryAndPage: function(query: string, pageNumber: number): string {
+    var apiKey = API_KEYS[this.state.queryNumber % API_KEYS.length];
+    if (query) {
+      return (
+        API_URL + 'movies.json?apikey=' + apiKey + '&q=' +
+        encodeURIComponent(query) + '&page_limit=20&page=' + pageNumber
+      );
+    } else {
+      // With no query, load latest movies
+      return (
+        API_URL + 'lists/movies/in_theaters.json?apikey=' + apiKey +
+        '&page_limit=20&page=' + pageNumber
+      );
+    }
+  },
+
+  searchMovies: function(query: string) {
+    this.timeoutID = null;
+
+    this.setState({filter: query});
+
+    var cachedResultsForQuery = resultsCache.dataForQuery[query];
+    if (cachedResultsForQuery) {
+      if (!LOADING[query]) {
+        this.setState({
+          dataSource: this.getDataSource(cachedResultsForQuery),
+          isLoading: false
+        });
+      } else {
+        this.setState({isLoading: true});
+      }
+      return;
+    }
+
+    LOADING[query] = true;
+    resultsCache.dataForQuery[query] = null;
+    this.setState({
+      isLoading: true,
+      queryNumber: this.state.queryNumber + 1,
+      isLoadingTail: false,
+    });
+
+    fetch(this._urlForQueryAndPage(query, 1))
+      .then((response) => response.json())
+      .catch((error) => {
+        LOADING[query] = false;
+        resultsCache.dataForQuery[query] = undefined;
+
+        this.setState({
+          dataSource: this.getDataSource([]),
+          isLoading: false,
+        });
+      })
+      .then((responseData) => {
+        LOADING[query] = false;
+        resultsCache.totalForQuery[query] = responseData.total;
+        resultsCache.dataForQuery[query] = responseData.movies;
+        resultsCache.nextPageNumberForQuery[query] = 2;
+
+        if (this.state.filter !== query) {
+          // do not update state if the query is stale
+          return;
+        }
+
+        this.setState({
+          isLoading: false,
+          dataSource: this.getDataSource(responseData.movies),
+        });
+      })
+      .done();
+  },
+});
