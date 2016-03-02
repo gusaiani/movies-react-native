2f84ba8c79cff77f2c36596be8df63d452e3c0b8
Gustavo Saiani <gustavo@poe.ma>
2016-03-02 10:14:33 -0300

    Start SearchScreen render methods

--- SearchScreen.js
+++ SearchScreen.js
@@ -134,4 +134,178 @@ var SearchScreen = React.createClass({
       })
       .done();
   },
+
+  hasMore: function(): boolean {
+    var query = this.state.filter;
+    if (!resultsCache.dataForQuery[query]) {
+      return true;
+    }
+    return (
+      resultsCache.totalForQuery[query] !==
+      resultsCache.dataForQuery[query].length
+    );
+  },
+
+  onEndReached: function() {
+    var query = this.state.filter;
+    if (!this.hasMore() || this.state.isLoadingTail) {
+      // We're already fetching or have all the elements so noop
+      return;
+    }
+
+    if (LOADING[query]) {
+      return;
+    }
+
+    LOADING[query] = true;
+    this.setState({
+      queryNumber: this.state.queryNumber + 1,
+      isLoadingTail: true,
+    });
+
+    var page = resultsCache.nextPageNumberForQuery[query];
+    invariant(page != null, 'Next page number for "%s" is missing', query);
+    fetch(this._urlForQueryAndPage(query, page))
+      .then((response) => response.json())
+      .catch((error) => {
+        console.error(error);
+        LOADING[query] = false;
+        this.setState({
+          isLoadingTail: false,
+        });
+      })
+      .then((responseData) => {
+        var moviesForQuery = resultsCache.dataForQuery[query].slice();
+
+        LOADING[query] = false;
+        // We reached the end of the list before the expected number of results
+        if (!responseData.movies) {
+          resultsCache.totalForQuery[query] = moviesForQuery.length;
+        } else {
+          for (var i in responseData.movies) {
+            moviesForQuery.push(responseData.movies[i]);
+          }
+          resultsCache.dataForQuery[query] = moviesForQuery;
+          resultsCache.nextPageNumberForQuery[query] += 1;
+        }
+
+        if (this.state.filter !== query) {
+          // do not update state if the query is stale
+          return;
+        }
+
+        this.setState({
+          isLoadingTail: false,
+          dataSource: this.getDataSource(resultsCache.dataForQuery[query]),
+        });
+      })
+      .done();
+  },
+
+  getDataSource: function(movies: Array<any>): ListView.DataSource {
+    return this.state.dataSource.cloneWithRows(movies);
+  },
+
+  selectMovie: function(movie: Object) {
+    if (Platform.OS === 'ios') {
+      this.props.navigator.push({
+        title: movie.title,
+        component: MovieScreen,
+        passProps: {movie},
+      });
+    } else {
+      dismissKeyboard();
+      this.props.navigator.push({
+        title: movie.title,
+        name: 'movie',
+        movie: movie,
+      });
+    }
+  },
+
+  onSearchChange: function(event: Object) {
+    var filter = event.nativeEvent.text.toLowerCase();
+
+    this.clearTimeout(this.timeoutID);
+    this.timeoutID = this.setTimeout(() => this.searchMovies(filter), 100);
+  },
+
+  renderFooter: function() {
+    if (!this.hasMore() || !this.state.isLoadingTail) {
+      return <View style={styles.scrollSpinner} />;
+    }
+    if (Platform.OS === 'ios') {
+      return <ActivityIndicatorIOS style={styles.scrollSpinner} />;
+    } else {
+      return (
+        <View  style={{alignItems: 'center'}}>
+          <ProgressBarAndroid styleAttr="Large"/>
+        </View>
+      );
+    }
+  },
+
+  renderSeparator: function(
+    sectionID: number | string,
+    rowID: number | string,
+    adjacentRowHighlighted: boolean
+  ) {
+    var style = styles.rowSeparator;
+    if (adjacentRowHighlighted) {
+        style = [style, styles.rowSeparatorHide];
+    }
+    return (
+      <View key={'SEP_' + sectionID + '_' + rowID}  style={style}/>
+    );
+  },
+
+  renderRow: function(
+    movie: Object,
+    sectionID: number | string,
+    rowID: number | string,
+    highlightRowFunc: (sectionID: ?number | string, rowID: ?number | string) => void,
+  ) {
+    return (
+      <MovieCell
+        key={movie.id}
+        onSelect={() => this.selectMovie(movie)}
+        onHighlight={() => highlightRowFunc(sectionID, rowID)}
+        onUnhighlight={() => highlightRowFunc(null, null)}
+        movie={movie}
+      />
+    );
+  },
+
+  render: function() {
+    var content = this.state.dataSource.getRowCount() === 0 ?
+      <NoMovies
+        filter={this.state.filter}
+        isLoading={this.state.isLoading}
+      /> :
+      <ListView
+        ref="listview"
+        renderSeparator={this.renderSeparator}
+        dataSource={this.state.dataSource}
+        renderFooter={this.renderFooter}
+        renderRow={this.renderRow}
+        onEndReached={this.onEndReached}
+        automaticallyAdjustContentInsets={false}
+        keyboardDismissMode="on-drag"
+        keyboardShouldPersistTaps={true}
+        showsVerticalScrollIndicator={false}
+      />;
+
+    return (
+      <View style={styles.container}>
+        <SearchBar
+          onSearchChange={this.onSearchChange}
+          isLoading={this.state.isLoading}
+          onFocus={() =>
+            this.refs.listview && this.refs.listview.getScrollResponder().scrollTo({ x: 0, y: 0 })}
+        />
+        <View style={styles.separator} />
+        {content}
+      </View>
+    );
+  },
 });
